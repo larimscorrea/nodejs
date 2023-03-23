7466f71b0c29e6389de7e1bb1143ff26550bb429
Larissa Corrẽa <larimscorrea@gmail.com>
2023-03-22 21:24:24 -0300

    mais um passo

---
+++ myfirst.js
@@ -0,0 +1,20 @@
+var http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Outra forma de escrever o de cima com menos código.
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World!');
+// }).listen(8080);
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
